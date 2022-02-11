a6c288c36cb24102b75065ee6c3743c125ce7eb0
Jeffrey Mesa <JeffreyArt@hotmail.com>
2022-02-11 12:13:48 -0400

    imp: upgrade angular 10 -> 11

--- microservices/webapp-service/src/app/pages/product-add/product-add.component.spec.ts
+++ microservices/webapp-service/src/app/pages/product-add/product-add.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductAddComponent } from './product-add.component';
 
@@ -6,7 +6,7 @@ describe('ProductAddComponent', () => {
   let component: ProductAddComponent;
   let fixture: ComponentFixture<ProductAddComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductAddComponent ]
     })

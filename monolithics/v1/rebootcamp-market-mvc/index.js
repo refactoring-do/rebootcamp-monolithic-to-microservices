51437ab64dc0772b7986ab568c60b271e9b7e81b
Jeffrey Mesa <JeffreyArt@hotmail.com>
2022-02-11 10:00:30 -0400

    imp: correct vulnerabilities by upgrading packages

--- monolithics/v1/rebootcamp-market-mvc/index.js
+++ monolithics/v1/rebootcamp-market-mvc/index.js
@@ -1,26 +1,26 @@
-const path = require("path");
-const express = require("express");
+const { PORT, APPLICATION_NAME } = require('./config');
+const { engine } = require('express-handlebars');
+const router = require('./routes/router');
+const express = require('express');
+const path = require('path');
 const server = express();
-const { PORT, APPLICATION_NAME } = require("./config");
-const router = require("./routes/router");
-const expressHbs = require("express-handlebars");
 
 server.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main",
-    partialsDir: "views/partials/",
-    extname: "hbs"
+  'hbs',
+  engine({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main',
+    partialsDir: 'views/partials/',
+    extname: 'hbs',
   })
 );
-server.set("view engine", "hbs");
-server.set("views", "views");
+server.set('view engine', 'hbs');
+server.set('views', 'views');
 
 server.use(express.urlencoded({ extended: false }));
-server.use(express.static(path.join(__dirname, "public")));
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.use("", router);
+server.use('', router);
 
 server.listen(PORT, () => {
   console.log(`${APPLICATION_NAME} running on port ${PORT}`);
